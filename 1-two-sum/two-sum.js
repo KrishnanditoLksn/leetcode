9f09cbbaea5aa2e74f5472cfcb80af325df7f9be
Emmanuel Krishnandito Laksana <96246664+KrishnanditoLksn@users.noreply.github.com>
2024-02-11 20:42:23 +0700

    Time: 113 ms (23.52%) | Memory: 49.8 MB (23.40%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+    let arr = []
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = 0; j < i; j++) {
+            if (nums[i] + nums[j] === target) {
+                return arr = [j,i]
+            }
+        }
+    }
+};
